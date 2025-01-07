0e9fe29bb8063eee63701aa40874a394bd138d76
Anshu Mukherjee <mukherja@wlhs.wlwv.k12.or.us>
2025-01-07 15:01:35 -0800

    update

--- src/HW1/Unit8HW1.java
+++ src/HW1/Unit8HW1.java
@@ -1,8 +1,18 @@
+package HW1;
+
+import java.util.Scanner;
 public class Unit8HW1{
 	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
 		//make your own 2D test array of integers (or whatever)
-
+		int[][] test = {
+				{2, 2, 12},
+				{3, 27, 32},
+				{23, 27, 36}
+		};
 		//ask user for column number
+		System.out.println("Enter a column: ");
+		int column = sc.nextInt();
 
 		//make sure it is a valid option
 
